ecafca2078c297c5a8da2cb142702cf7da122beb
monkeyloko <defesanti@gmail.com>
2023-09-27 00:01:59 -0300

    avance...

---
+++ catalogo/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import ReactDOM from "react-dom"; // Import ReactDOM from "react-dom"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from './components/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
